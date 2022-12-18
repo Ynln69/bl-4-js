09e8e5eda77bf88c6b182f64d24ac30f2c47d20b
Yana <yanalunina96@gmail.com>
2022-12-18 11:01:56 +0200

    task1

---
+++ index.js
@@ -0,0 +1,14 @@
+//1. Використовуючи функцію if...else,
+//напишіть код, який буде запитувати:
+//"What is the official name of JavaScript?"
+//Якщо користувач ввів "ECMAScript",
+//то показувати через alert: "True!"
+//у противному випадку показати:"Don't you know? ECMAScript!"
+
+// const text = 'ECMAScript';
+// const answer = prompt('What is the official name of JavaScript?');
+// if (answer === text) {
+//     alert('True!');
+// } else {
+//     alert("Don't you know? ECMAScript!");
+// }
