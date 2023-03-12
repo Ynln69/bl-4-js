4120a670b83e501d2ec6955340c0306db8ce3274
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-12 13:12:40 +0200

    add class

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,46 @@
 import axios from "axios";
 
-const BASE_URL = "https://dummyjson.com";
+// const BASE_URL = "https://dummyjson.com";
 
-export const apiDummyJson = axios.create({
-  baseURL: BASE_URL,
+// // export const apiDummyJson = axios.create({
+//   baseURL: BASE_URL,
   // timeout: 500,
-});
+// });
+export class ApiDummyJson {
+#BASE_URL = "https://dummyjson.com";
+constructor() {
+    this.instance = axios.create({
+      baseURL: this.#BASE_URL, 
+      // timeout: 500,
+    });
+}
+
+async getProducts() {
+  try {
+    return await this.instance.get(`/products`);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async getProductById(id) {
+  try {
+    return await this.instance.get(`/products/${id}`);
+  } catch (error) {
+    console.log(error);
+    if(error.response.status === 404) {
+      alert('Enter other id more than 100!');
+    }
+    console.log(error.message);
+  }
+}
+
+async createProduct(newProduct) {
+  try {
+    return await this.instance.post(`/products/add`, newProduct);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+}
+
