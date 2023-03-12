cf5306e6050b8a57f87d25e8fa3431f09d7dfbb3
Olga007 <o.piankova@ukr.net>
2023-03-12 11:55:27 +0200

    task2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import { getProducts } from "./requests/products";
-import { createMarkupProduct } from "./services/markupService";
+import { getProducts, getProductById } from "./requests/products";
+import { createMarkupProducts, createMarkupProduct } from "./services/markupService";
 
 import "./styles/normalize.css";
 import "./styles/index.css";
 
 // console.log(getProducts());
 const productsList = document.querySelector("#allProducts");
+const singleProductForm = document.querySelector('#singleProductForm');
+const singleProduct = document.querySelector('#singleProduct');
 
-async function addAllProducts() {
-  const {
-    data: { products },
-  } = await getProducts();
-  createMarkupProduct(products, productsList);
-}
+singleProductForm.addEventListener('submit', findProductById);
 
-addAllProducts();
+// async function addAllProducts() {
+//   const {
+//     data: { products },
+//   } = await getProducts();
+//   createMarkupProducts(products, productsList);
+// }
+
+// addAllProducts();
+
+
+async function findProductById(evt) {
+  evt.preventDefault();
+
+  const productId = evt.target.elements.id.value.trim();
+  if(!productId) {
+    return;
+  }
+  const {data} = await getProductById(productId);
+  createMarkupProduct(data, singleProduct);
+};
--- src/requests/products.js
+++ src/requests/products.js
@@ -3,7 +3,19 @@ import { apiDummyJson } from "../services/api";
 export async function getProducts() {
   try {
     return await apiDummyJson.get(`/products`);
-  } catch (e) {
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function getProductById(id) {
+  try {
+    return await apiDummyJson.get(`/products/${id}`);
+  } catch (error) {
+    console.log(error);
+    if(error.response.status === 404) {
+      alert('Enter other id more than 100!');
+    }
     console.log(error.message);
   }
 }
